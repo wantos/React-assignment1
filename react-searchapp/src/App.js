4f90527a8acb124e0b1fa1e7d31b255001c3daf5
Wantono-Emmanuel <emmasirabo78@gmail.com>
2020-10-01 22:36:10 +0300

    added search files

---
+++ react-searchapp/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Search from './components/Search';
+
+export default class App extends component {
+
+    render() {
+      return (
+        <div>
+          <Search/>
+        </div>
+      )
+    }
+}
---
+++ react-searchapp/src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './search.css'
+
+export default class Search extends Component {
+    state={output:""}
+    render() {
+        return (
+            <div>
+                <h1>MY SEARCH APP by Emma</h1>
+                <div className="FormSearch">
+                    <form>
+                        <input type="text" placeholder="Search.." onChange={e=>this.setState({output: e.target.value})}/>
+
+                    </form>
+                  <p>{this.state.output}</p>
+                </div>
+            </div>
+        )
+    }
+}
